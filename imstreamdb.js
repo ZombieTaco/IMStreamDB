b53a28452138e0eedb9e1542d475444826c5bda4
Literalmeme <ZombieTaco@users.noreply.github.com>
2018-02-28 16:16:25 +1300

    Update imstreamdb.js

--- imstreamdb.js
+++ imstreamdb.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IMStreamDB
 // @namespace    http://imdb.com/
-// @version      1.0.0
+// @version      2.0.0
 // @description  If source, adds stream to the movie on the IMDB page
 // @author       ZombieTaco
 // @include      http://www.imdb.com/*
